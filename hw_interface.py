1a1b049daa0b5ae7298d4b56c55c13583ce133b5
-T.K.- <t_k_233@outlook.com>
2022-01-21 15:20:25 -0800

    FIX (hw): change to CommNet lib

--- hw_interface.py
+++ hw_interface.py
@@ -22,67 +22,53 @@ mux_l = 0
 mux_r = 0
 
 def getButtonState():
-	buf = bus.read_byte(0x20)
-	
-	buttons[5] = 1 - ((buf >> 7) & 1)
-	buttons[6] = 1 - ((buf >> 6) & 1)
-	
-	if (buf >> 5) & 1 == 0:
-		mux_r = 3
-	elif (buf >> 4) & 1 == 0:
-		mux_r = 2
-	elif (buf >> 3) & 1 == 0:
-		mux_r = 1
-	else:
-		mux_r = 0
-		
-	if (buf >> 2) & 1 == 0:
-		mux_l = 3
-	elif (buf >> 1) & 1 == 0:
-		mux_l = 2
-	elif (buf >> 0) & 1 == 0:
-		mux_l = 1
-	else:
-		mux_l = 0
-	
-	buf = bus.read_byte(0x21)
-	
-	buttons[4] = 1 - ((buf >> 7) & 1)
-	buttons[3] = 1 - ((buf >> 6) & 1)
-	buttons[2] = 1 - ((buf >> 5) & 1)
-	buttons[1] = 1 - ((buf >> 4) & 1)
-	buttons[0] = 1 - ((buf >> 3) & 1)
-	buttons[9] = 1 - ((buf >> 2) & 1)
-	buttons[8] = 1 - ((buf >> 1) & 1)
-	buttons[7] = 1 - ((buf >> 0) & 1)
-	
-	
-	return {"/wheel/buttons": buttons, "/wheel/mux_l": mux_l, "/wheel/mux_r": mux_r}
+    buf = bus.read_byte(0x20)
+    
+    buttons[5] = 1 - ((buf >> 7) & 1)
+    buttons[6] = 1 - ((buf >> 6) & 1)
+    
+    if (buf >> 5) & 1 == 0:
+        mux_r = 3
+    elif (buf >> 4) & 1 == 0:
+        mux_r = 2
+    elif (buf >> 3) & 1 == 0:
+        mux_r = 1
+    else:
+        mux_r = 0
+        
+    if (buf >> 2) & 1 == 0:
+        mux_l = 3
+    elif (buf >> 1) & 1 == 0:
+        mux_l = 2
+    elif (buf >> 0) & 1 == 0:
+        mux_l = 1
+    else:
+        mux_l = 0
+    
+    buf = bus.read_byte(0x21)
+    
+    buttons[4] = 1 - ((buf >> 7) & 1)
+    buttons[3] = 1 - ((buf >> 6) & 1)
+    buttons[2] = 1 - ((buf >> 5) & 1)
+    buttons[1] = 1 - ((buf >> 4) & 1)
+    buttons[0] = 1 - ((buf >> 3) & 1)
+    buttons[9] = 1 - ((buf >> 2) & 1)
+    buttons[8] = 1 - ((buf >> 1) & 1)
+    buttons[7] = 1 - ((buf >> 0) & 1)
+    
+    
+    return buttons, mux_l, mux_r
 
 
-import socket
-import json
+from birge_comm_net import CommNetCore
 import time
 
-
-data = {"method": "set", "data": {}}
-
-def startConnection():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("10.20.22.1", 2400))
-    return s
-
-s = startConnection()
+client = CommNetCore()
+client.connect(("10.20.22.1", 2400))
 
 while True:
-    data["data"] = getButtonState()
-
-    try:
-        s.send(json.dumps(data).encode())
-    except ConnectionResetError or ConnectionRefusedError as e:
-        print(e)
-        s.close()
-        s = startConnection()
-
-    print(data)
-    time.sleep(0.1)
+    buttons, mux_l, mux_r = getButtonState()
+    client.set("/wheel/buttons", buttons)
+    client.set("/wheel/mux_l", mux_l)
+    client.set("/wheel/mux_r", mux_r)
+    time.sleep(0.033)
